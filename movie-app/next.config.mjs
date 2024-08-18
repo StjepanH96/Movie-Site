0aaa09b37899734085774167fa690666f11144eb
StjepanH96 <stjepan.hecimovic@hotmail.com>
2024-08-18 13:17:41 +0200

    fix api key definition

--- movie-app/next.config.mjs
+++ movie-app/next.config.mjs
@@ -5,6 +5,7 @@ dotenv.config();
 const nextConfig = {
   env: {
     TMDB_API_KEY: process.env.TMDB_API_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
   },
   compiler: {
     styledComponents: true
